5b2a606000c478a3f88e51bbc0e3e2ccfd9f0cdf
Marko Krizan <markokrizan64@gmail.com>
2022-08-04 22:47:37 +0200

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Router } from "svelte-navigator";
+	import Sidebar from "./components/Sidebar.svelte";
+	import Routes from "./Routes.svelte";
+</script>
+
+<Router>
+	<main class="h-100">
+		<div class="container-fluid h-100">
+			<div class="row h-100">
+				<div class="col-2 p-0">
+					<Sidebar />
+				</div>
+				<div class="col-10">
+					<Routes />
+				</div>
+			</div>
+		</div>
+	</main>
+</Router>
---
+++ src/Routes.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Route } from "svelte-navigator";
+	import SingleCourse from "./pages/SingleCourse.svelte";
+</script>
+
+<Route path="courses">
+    <h3>Courses</h3>
+</Route>
+
+<Route path="courses/*id" component={SingleCourse} />
+
+<Route path="exams">
+    <h3>Exams</h3>
+</Route>
+
+<Route path="teachers">
+    <h3>Teachers</h3>
+</Route>
+
+<Route path="terms">
+    <h3>Terms</h3>
+</Route>
+
+<Route>
+    <h3>Default dashboard route</h3>
+</Route>
+
