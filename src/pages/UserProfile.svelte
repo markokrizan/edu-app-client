4d6ad9349bc9c3bce809818e73fe652d04551884
Marko Krizan <markokrizan64@gmail.com>
2022-11-14 20:45:16 +0100

    Basic profile page

--- src/pages/UserProfile.svelte
+++ src/pages/UserProfile.svelte
@@ -1,7 +1,33 @@
 <script lang="ts">
     import PrivateLayout from "../layouts/PrivateLayout.svelte";
-
+    import { userStore } from "../store";
+    
+    const user = userStore.getUser();
 </script>
+ 
 <PrivateLayout>
-    <h1>User profile page (TODO)</h1>
+    <div class="row">
+        <h2>{$user?.firstName} {$user?.lastName}</h2>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-4">
+            {#if $user.profileImage}
+                <img src="{$user.profileImage}" class="img-fluid mb-3" alt="User profile">
+            {:else}
+                (No image availabe)
+            {/if}
+
+            <dl class="row mt-1">
+                <dt class="col-sm-4">Email:</dt>
+                <dd class="col-sm-8">{$user?.email}</dd>
+    
+                <dt class="col-sm-4">Phone number:</dt>
+                <dd class="col-sm-8">{$user.phoneNumber}</dd>
+    
+                <dt class="col-sm-4">Roles:</dt>
+                <dd class="col-sm-8">{$user?.roles?.map?.(role => role.name).join(', ')}</dd>
+            </dl>
+        </div>
+    </div>    
 </PrivateLayout>
