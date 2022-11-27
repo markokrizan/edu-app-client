ec561d47d6f809299af56ba9a9045d46e176f7bb
Marko Krizan <markokrizan64@gmail.com>
2022-11-27 21:00:38 +0100

    Load dynamic data for single teacher page

--- src/components/teacher/Teacher.svelte
+++ src/components/teacher/Teacher.svelte
@@ -11,7 +11,7 @@
 
 <div class="row">
     <div class="col-md-4">   
-        <img src="{teacher.profileImage}" class="img-fluid mb-3" alt="Teacher profile">
+        <img src="{teacher.profileImage || '/assets/user-profile-icon.jpg'}" class="img-fluid mb-3" alt="Teacher profile">
 
         <dl class="row">
             <dt class="col-sm-4">Email:</dt>
@@ -24,21 +24,23 @@
             <dd class="col-sm-8">{teacher.academicTitle}</dd>
         </dl>
     </div>
-    <div class="col-md-8">
-        <dt class="mb-2">Engagements:</dt>
-    <dd>
-        <ul class="list-group w-50">
-            {#each teacher.engagements as engagement}
-                <HoverableInteraction>
-                    <Link 
-                        to="/courses/{engagement.course.id}" 
-                        class="text-decoration-none text-dark" 
-                    >
-                        <li class="list-group-item">{engagement.course.name} - {engagement.engagementType}</li>
-                    </Link>
-                </HoverableInteraction>
-            {/each}
-        </ul>
-    </dd>
-    </div>
+    {#if teacher.engagements?.length}
+        <div class="col-md-8">
+            <dt class="mb-2">Engagements:</dt>
+            <dd>
+                <ul class="list-group w-50">
+                    {#each teacher.engagements as engagement}
+                        <HoverableInteraction>
+                            <Link 
+                                to="/courses/{engagement.course.id}" 
+                                class="text-decoration-none text-dark" 
+                            >
+                                <li class="list-group-item">{engagement.course.name} - {engagement.engagementType}</li>
+                            </Link>
+                        </HoverableInteraction>
+                    {/each}
+                </ul>
+            </dd>
+        </div>
+    {/if}
 </div>
--- src/pages/Teacher.svelte
+++ src/pages/Teacher.svelte
@@ -1,50 +1,19 @@
 <script>
+    import Loadable from "../components/common/Loadable.svelte";
     import Teacher from "../components/teacher/Teacher.svelte";
     import PrivateLayout from "../layouts/PrivateLayout.svelte";
+    import httpService from "../services/httpService";
 
     export let id;
-
-    const DUMMY_TEACHER = {
-        id: id,
-        firstName: "Marko",
-        lastName: "Markovic",
-        username: "markom",
-        email: "marko@mail.com",
-        phoneNumber: "123123",
-        personalIdNumber: "111111",
-        roles: {
-            id: 1,
-            name: "ROLE_TEACHER",
-        },
-        academicTitle: "PhD",
-        profileImage: "/assets/user-profile-icon.jpg",
-        engagements: [
-            {
-                id: 1,
-                engagementType: "PROFESSOR",
-                course: {
-                    id: id,
-                    name: "DB Administration",
-                    year: "FIRST",
-                    semester: "SUMMER",
-                    espbPoints: 4
-                },
-            },
-            {
-                id: 1,
-                engagementType: "ASSISTANT",
-                course: {
-                    id: 2,
-                    name: 'Web development',
-                    year: 'THIRD',
-                    semester: 'WINTER',
-                    espbPoints: 6
-                },
-            },
-        ],
-    };
 </script>
 
 <PrivateLayout>
-    <Teacher teacher={DUMMY_TEACHER} />
+    <Loadable
+        fetchFn={() => httpService.withAuth().request({ method: 'GET', url: `api/teachers/${id}`})}
+        queryKey="teacher"
+    >
+        <svelte:fragment slot="data" let:data>
+            <Teacher teacher={data}/>
+        </svelte:fragment>
+    </Loadable>
 </PrivateLayout>
