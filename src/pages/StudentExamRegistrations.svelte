ef0f8232a4367821ee96494d6d59efa7372298a3
Marko Krizan <markokrizan@Markos-MacBook-Pro.local>
2023-01-15 17:10:26 +0100

    Exam registration - student perspective - initial version

--- src/pages/StudentExamRegistrations.svelte
+++ src/pages/StudentExamRegistrations.svelte
@@ -1,20 +1,89 @@
 <script>
+    import { useQueryClient } from "@sveltestack/svelte-query";
+    import { Link } from "svelte-navigator";
     import Pager from "../components/common/Pager.svelte";
-    import ExamTable from "../components/exam/ExamTable.svelte";
+    import Table from "../components/common/Table.svelte";
     import PrivateLayout from "../layouts/PrivateLayout.svelte";
     import httpService from "../services/httpService";
     import { userStore } from "../store";
-
+    
+    const queryClient = useQueryClient();
     const user = userStore.getUser();
+
+    const registeredExamsColumnNames = ['Id', 'Exam date', 'Location', 'Term', 'Course'];
+    const registrableExamColumnNames = ['Id', 'Exam date', 'Location', 'Term', 'Course', 'Register'];
+
+    const fetchRegisteredExams = (page) => {
+        return httpService
+            .withAuth()
+            .request({ method: 'GET', url: `api/students/${$user.id}/exam-registrations?page=${page}`});
+    }
 
     const fetchRegistrableExams = (page) => {
         return httpService
             .withAuth()
             .request({ method: 'GET', url: `api/students/${$user.id}/registrable-exams?page=${page}`});
     }
+
+    const examRegistrationsToRegisteredTableRow = ({ exam }) => [exam.id, exam.examDate, exam.location, exam?.term?.name, exam?.course];
+    const examsToRegistableTableRow = (exam) => [exam.id, exam.examDate, exam.location, exam.term.name, exam.course, { examId: exam.id }];
+
+    // TODO: Reuse through component
+    const reducePagesToTableRows = (pages, rowMaper) => {
+        return pages.reduce(
+            (acc, page) => {
+                return [...acc, ...page.content.map(item => rowMaper(item))]
+            },
+            []
+        )
+    }
+
+    const handleRegister = async (examId) => {
+        const registration = {
+            exam: {
+                id: examId
+            },
+            student: {
+                id: $user.id
+            },
+            examRegistrationStatus: 'REGISTERED'
+        }
+
+        await httpService.withAuth()
+            .request({ method: 'POST', url: 'api/exam-registrations', body: JSON.stringify(registration)});
+        
+        window.location.reload(); // A dirty hack in order not to deal with the infinite query cache reload issue now
+    }
 </script>
 
 <PrivateLayout>
+    <h4>Registered exams</h4>
+
+    <Pager
+        fetchFn={fetchRegisteredExams} 
+        queryKey="registered-exams"
+        queryOptions={{ refetchOnMount: false }}
+    >
+        <svelte:fragment slot="pages" let:pages>
+            <Table columnNames={registeredExamsColumnNames} tableData={reducePagesToTableRows(pages, examRegistrationsToRegisteredTableRow)}>
+                <svelte:fragment slot="row" let:cellValue let:index>
+                    {#if index === 4}
+                        <td>
+                            <Link
+                                to="/courses/{cellValue?.id}"
+                                class="text-decoration-none text-dark" 
+                            >
+                                <span class="fw-bold">{cellValue?.name}</span>
+                            </Link>
+                        </td>
+                    {:else}
+                        <td>{cellValue}</td>
+                    {/if}
+                </svelte:fragment>
+            </Table>
+        </svelte:fragment>
+    </Pager>
+
     <h4>Registrable exams</h4>
 
     <Pager
@@ -23,7 +92,28 @@
         queryOptions={{ refetchOnMount: false }}
     >
         <svelte:fragment slot="pages" let:pages>
-            <ExamTable examPages={pages} />
+            <Table columnNames={registrableExamColumnNames} tableData={reducePagesToTableRows(pages, examsToRegistableTableRow)}>
+                <svelte:fragment slot="row" let:cellValue let:index>
+                    {#if index === 4}
+                        <td>
+                            <Link
+                                to="/courses/{cellValue.id}"
+                                class="text-decoration-none text-dark" 
+                            >
+                                <span class="fw-bold">{cellValue.name}</span>
+                            </Link>
+                        </td>
+                    {:else if index === 5}
+                        <td>
+                            <button class="btn btn-primary" on:click={() => handleRegister(cellValue.examId)}>
+                                <i class="bi bi-pen"></i>
+                            </button>
+                        </td>
+                    {:else}
+                        <td>{cellValue}</td>
+                    {/if}
+                </svelte:fragment>
+            </Table>
         </svelte:fragment>
     </Pager>
 </PrivateLayout>
