e1d22d1293de1eb14afe125a3e42833ca5958a16
Marko Krizan <markokrizan64@gmail.com>
2023-04-02 23:13:21 +0200

    Implement listing and downloading student documents

---
+++ src/components/student/Documents.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import httpService from "../../services/httpService";
+
+  export let student;
+</script>
+
+{#if student.documents?.length}
+  <dt class="mb-2">Documents:</dt>
+  <dd>
+    <ul class="list-group w-50">
+      {#each student.documents as document}
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          <span>{document.name}</span>
+          <button
+            class="btn btn-primary w-50 ms-2"
+            on:click={() =>
+              httpService.download(
+                `api/students/${student.id}/documents/${document.id}/download`,
+                document.name
+              )}
+          >
+            Download
+          </button>
+        </li>
+      {/each}
+    </ul>
+  </dd>
+{:else}
+  <p>No documents yet</p>
+{/if}
--- src/components/student/Student.svelte
+++ src/components/student/Student.svelte
@@ -1,55 +1,64 @@
 <script lang="ts">
-    import httpService from "../../services/httpService";
-    import Grades from "./Grades.svelte";
+  import httpService from "../../services/httpService";
+  import Documents from "./Documents.svelte";
+  import Grades from "./Grades.svelte";
 
-    export let student;
+  export let student;
 </script>
 
 <div class="row">
-    <div class="d-flex justify-content-between">
-        <h4>{student.firstName} {student.lastName}</h4>
-        <slot name="action"/>
-    </div>
+  <div class="d-flex justify-content-between">
+    <h4>{student.firstName} {student.lastName}</h4>
+    <slot name="action" />
+  </div>
 </div>
 
 <div class="row">
-    <div class="col-md-4">
-        <img
-            src={student.profileImage || "/assets/user-profile-icon.jpg"}
-            class="img-fluid mb-3"
-            alt="Student profile"
-        />
+  <div class="col-md-4">
+    <img
+      src={student.profileImage || "/assets/user-profile-icon.jpg"}
+      class="img-fluid mb-3"
+      alt="Student profile"
+    />
 
-        <dl class="row">
-            <dt class="col-sm-4">Study Program:</dt>
-            <dd class="col-sm-8">{student?.studyProgram?.name}</dd>
+    <dl class="row">
+      <dt class="col-sm-4">Study Program:</dt>
+      <dd class="col-sm-8">{student?.studyProgram?.name}</dd>
 
-            <dt class="col-sm-4">Email:</dt>
-            <dd class="col-sm-8">{student.email}</dd>
+      <dt class="col-sm-4">Email:</dt>
+      <dd class="col-sm-8">{student.email}</dd>
 
-            <dt class="col-sm-4">Phone number:</dt>
-            <dd class="col-sm-8">{student.phoneNumber}</dd>
+      <dt class="col-sm-4">Phone number:</dt>
+      <dd class="col-sm-8">{student.phoneNumber}</dd>
 
-            <dt class="col-sm-4">Financial status:</dt>
-            <dd class="col-sm-8">{student.financialStatus}</dd>
+      <dt class="col-sm-4">Financial status:</dt>
+      <dd class="col-sm-8">{student.financialStatus}</dd>
 
-            <dt class="col-sm-4">Current study year:</dt>
-            <dd class="col-sm-8">{student.currentStudyYear}</dd>
+      <dt class="col-sm-4">Current study year:</dt>
+      <dd class="col-sm-8">{student.currentStudyYear}</dd>
 
-            <dt class="col-sm-4">School id number:</dt>
-            <dd class="col-sm-8">{student.schoolIdNumber}</dd>
-        </dl>
-        <div class="row">
-            <button
-                class="btn btn-primary w-50 ms-2"
-                on:click={() => httpService.download(`api/students/${student.id}/report`, 'student-report.pdf')}>
-                    Download Report
-                </button>
-        </div>
+      <dt class="col-sm-4">School id number:</dt>
+      <dd class="col-sm-8">{student.schoolIdNumber}</dd>
+    </dl>
+    <div class="row">
+      <button
+        class="btn btn-primary w-50 ms-2"
+        on:click={() =>
+          httpService.download(
+            `api/students/${student.id}/report`,
+            "student-report.pdf"
+          )}
+      >
+        Download Report
+      </button>
     </div>
-    <div class="col-md-8">
-        <div class="row">
-            <Grades grades={student.grades} />
-        </div>
+  </div>
+  <div class="col-md-8">
+    <div class="row">
+      <Grades grades={student.grades} />
     </div>
+    <div class="row">
+      <Documents student={student} />
+    </div>
+  </div>
 </div>
